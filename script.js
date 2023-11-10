57159e6d2fb76d8e6d24b68d29136085a6d4ab2f
samedfft2634 <samedfft@gmail.com>
2023-11-10 12:38:42 +0300

    profit card

---
+++ script.js
@@ -0,0 +1,7 @@
+// tarihInput.valueAsDate = new Date()
+
+window.addEventListener("load", () => {
+    const tarihInput = document.getElementById("tarihInput");
+  
+    tarihInput.valueAsDate = new Date();
+  });
